4d50af9a562522cf8a3689b482ee72a1d1582f0a
sdezone <mh.nusretkaya@hotmail.com>
2023-08-26 12:37:37 +0300

    refs #100011 updated database and created first migration.

---
+++ Api/Infrastructure/Sozluk.Infrastructure.Persistence/Migrations/20230826092246_initialCreate.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sozluk.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "email_confirmation",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    OldEmailAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    NewEmailAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_email_confirmation", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirsName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    EmailAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    EmailConfirmed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Subject = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Entries_user_CreatedById",
+                        column: x => x.CreatedById,
+                        principalSchema: "dbo",
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entry_comment",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EntryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_entry_comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_entry_comment_Entries_EntryId",
+                        column: x => x.EntryId,
+                        principalTable: "Entries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_entry_comment_user_CreatedById",
+                        column: x => x.CreatedById,
+                        principalSchema: "dbo",
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entry_favorite",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EntryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_entry_favorite", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_entry_favorite_Entries_EntryId",
+                        column: x => x.EntryId,
+                        principalTable: "Entries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_entry_favorite_user_CreatedById",
+                        column: x => x.CreatedById,
+                        principalSchema: "dbo",
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entry_vote",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EntryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    VoteType = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_entry_vote", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_entry_vote_Entries_EntryId",
+                        column: x => x.EntryId,
+                        principalTable: "Entries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entry_comment_favorite",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EntryCommentId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_entry_comment_favorite", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_entry_comment_favorite_entry_comment_EntryCommentId",
+                        column: x => x.EntryCommentId,
+                        principalSchema: "dbo",
+                        principalTable: "entry_comment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_entry_comment_favorite_user_CreatedById",
+                        column: x => x.CreatedById,
+                        principalSchema: "dbo",
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entry_comment_vote",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EntryCommentId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "TEXT", nullable: false),
+                    VoteType = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_entry_comment_vote", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_entry_comment_vote_entry_comment_EntryCommentId",
+                        column: x => x.EntryCommentId,
+                        principalSchema: "dbo",
+                        principalTable: "entry_comment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entries_CreatedById",
+                table: "Entries",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_comment_CreatedById",
+                schema: "dbo",
+                table: "entry_comment",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_comment_EntryId",
+                schema: "dbo",
+                table: "entry_comment",
+                column: "EntryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_comment_favorite_CreatedById",
+                schema: "dbo",
+                table: "entry_comment_favorite",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_comment_favorite_EntryCommentId",
+                schema: "dbo",
+                table: "entry_comment_favorite",
+                column: "EntryCommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_comment_vote_EntryCommentId",
+                schema: "dbo",
+                table: "entry_comment_vote",
+                column: "EntryCommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_favorite_CreatedById",
+                schema: "dbo",
+                table: "entry_favorite",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_favorite_EntryId",
+                schema: "dbo",
+                table: "entry_favorite",
+                column: "EntryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entry_vote_EntryId",
+                schema: "dbo",
+                table: "entry_vote",
+                column: "EntryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "email_confirmation",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "entry_comment_favorite",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "entry_comment_vote",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "entry_favorite",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "entry_vote",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "entry_comment",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Entries");
+
+            migrationBuilder.DropTable(
+                name: "user",
+                schema: "dbo");
+        }
+    }
+}
