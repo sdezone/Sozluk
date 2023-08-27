e1618def4baf5f7fae5d3e1ac66a793d870758cc
sdezone <mh.nusretkaya@hotmail.com>
2023-08-27 15:17:33 +0300

    refs #100024 created usercontroller for login.

---
+++ Api/WebApi/Sozluk.Api.WebApi/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sozluk.Common.Models.RequestModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sozluk.Api.WebApi
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public UserController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody]LoginUserCommand loginUserCommand)
+        {
+            var res = await mediator.Send(loginUserCommand);
+            return Ok(res);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
